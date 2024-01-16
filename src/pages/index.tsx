4d4b41cc933bf3d5e31c0845cf29185d524390af
pandyama <pandyama2@gmail.com>
2024-01-15 21:27:03 -0700

    making use of textInput component on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import type { PageProps } from "gatsby";
 import Button from "../components/ui/button";
+import TextInput from "../components/ui/textInput";
 
 const mainStyles = {
   display: "flex",
-  justifyContent: "center",
-  padding: 50,
+  flexDirection: "column",
+  alignItems: "center",
+  padding: 30,
 };
 
 const tableStyles = {
@@ -18,45 +20,63 @@ const tableRowStyles = {
 };
 
 const tableDataStyles = {
-  // border: `1px solid #000000`,
+  border: `1px solid #000000`,
   textalign: `center`,
   padding: 20,
 };
 
+const header = {
+  fontFamily: `Inter, -apple-system, system-ui, "Segoe UI", Helvetica, Arial,
+  sans-serif`,
+  fontWeight: 400,
+  textTransform: `Uppercase`,
+};
+
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={mainStyles}>
-      <table style={tableStyles}>
-        <tr style={tableRowStyles}>
-          <th></th>
-          <th>Primary</th>
-          <th>Outline</th>
-        </tr>
-        <tr style={tableRowStyles}>
-          <td style={tableDataStyles}>Default</td>
-          <td style={tableDataStyles}>
-            <Button variant="Primary">Primary</Button>
-          </td>
-          <td style={tableDataStyles}>
-            <Button variant="Outline">Outline</Button>
-          </td>
-        </tr>
-        <tr style={tableRowStyles}>
-          <td style={tableDataStyles}>Danger</td>
-          <td style={tableDataStyles}>
-            <Button variant="Primary" danger="Danger">
-              Primary
-            </Button>
-          </td>
+    <>
+      {/* https://github.com/cssinjs/jss/issues/1344 */}
+      <div style={mainStyles as React.CSSProperties}>
+        <h3 style={header as React.CSSProperties}>Button Component</h3>
+        <table style={tableStyles}>
+          <tr style={tableRowStyles}>
+            <th></th>
+            <th>Primary</th>
+            <th>Outline</th>
+          </tr>
+          <tr style={tableRowStyles}>
+            <td style={tableDataStyles}>Default</td>
+            <td style={tableDataStyles}>
+              <Button variant="Primary">Primary</Button>
+            </td>
+            <td style={tableDataStyles}>
+              <Button variant="Outline">Outline</Button>
+            </td>
+          </tr>
+          <tr style={tableRowStyles}>
+            <td style={tableDataStyles}>Danger</td>
+            <td style={tableDataStyles}>
+              <Button variant="Primary" danger="Danger">
+                Primary
+              </Button>
+            </td>
 
-          <td style={tableDataStyles}>
-            <Button variant="Outline" danger="Danger">
-              Outline
-            </Button>
-          </td>
-        </tr>
-      </table>
-    </main>
+            <td style={tableDataStyles}>
+              <Button variant="Outline" danger="Danger">
+                Outline
+              </Button>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <hr />
+      <div style={mainStyles as React.CSSProperties}>
+        <h3 style={header as React.CSSProperties}>Text Input Component</h3>
+        <TextInput label="email" placeholder="Enter Email"></TextInput>
+        <br />
+        <TextInput label="password" placeholder="Enter Password"></TextInput>
+      </div>
+    </>
   );
 };
 
